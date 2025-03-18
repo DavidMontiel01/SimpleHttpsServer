6cdabd09f519ea42da40bae0e965de628ccb8bdf
David Montiel <davidmontiel549@gmail.com>
2025-03-18 05:05:36 -0700

    Add java Configuration files

---
+++ src/main/java/com/SimpleHttpsServer/config/ConfigurationManager.java
@@ -0,0 +1,76 @@
+package com.SimpleHttpsServer.config;
+
+import com.SimpleHttpsServer.util.Json;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ConfigurationManager {
+    //Only need one Config manager at all times
+    private static ConfigurationManager myConfigurationManager;
+    private static Configuration myCurrentConfiguration;
+
+    private ConfigurationManager() {
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public static ConfigurationManager getInstance(){
+        if(myConfigurationManager == null){
+            myConfigurationManager = new ConfigurationManager();
+        }
+        return myConfigurationManager;
+    }
+
+    /**
+     * Used to load a Configuration by the path provided
+     * @param filePath
+     */
+    public void loadConfigurationFile(String filePath) {
+        FileReader fr = null;
+        try {
+            fr = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfigurationException(e);
+        }
+        StringBuffer sb = new StringBuffer();
+
+        int i;
+        try {
+            while(( i = fr.read()) != -1){
+                sb.append((char)i);
+            }
+        } catch (IOException e) {
+            throw new HttpConfigurationException(e);
+        }
+
+        JsonNode conf = null;
+        try {
+            conf = Json.parse(sb.toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Error parsing the configuration file");
+        }
+
+        try {
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error parsing the configuration file, Internal");
+        }
+    }
+
+    /**
+     *  returns the current loaded Configuration
+     */
+    public Configuration getCurrentConfiguration() {
+        if(myCurrentConfiguration == null){
+            throw new HttpConfigurationException("No Current Configuration Set");
+        }
+        return myCurrentConfiguration;
+    }
+}
