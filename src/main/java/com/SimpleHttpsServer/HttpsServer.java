e232dcb2a056d7ad7bdad0f857b3a9be75362f4a
David Montiel <davidmontiel549@gmail.com>
2025-03-18 03:35:35 -0700

    Inital Commit

---
+++ src/main/java/com/SimpleHttpsServer/HttpsServer.java
@@ -0,0 +1,12 @@
+package com.SimpleHttpsServer;
+/**
+ * Driver Class for Https Server
+ */
+public class HttpsServer {
+
+    public static void main(String[] args){
+        //forDebugging
+        System.out.println("Server starting...");
+
+    }
+}
